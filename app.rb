639543001adf9ab47ade8364a71c339f5f6865fc
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-01-26 19:43:06 +0000

    Changed to quotes generator

--- app.rb
+++ app.rb
@@ -2,21 +2,16 @@ require "sinatra"
 require "sinatra/reloader"
 require "http"
 
-def get_random_anime_list
-  api_url = "https://api.jikan.moe/v4/random/anime"
-  anime_list = []
-
-  3.times do
-    data = HTTP.get(api_url)
-    parsed_data = JSON.parse(data)
-    anime_data = parsed_data["data"]
-    anime_list.push(anime_data)
-  end
-  return anime_list
+def get_random_quote_list
+  api_url = "https://api.quotable.io/quotes/random?limit=5"
+  data = HTTP.get(api_url)
+  parsed_data = JSON.parse(data)
+  
+  return parsed_data
 
 end
 
 get("/") do
-  @anime_list = get_random_anime_list
+  @quote_list = get_random_quote_list
   erb(:random)
 end
