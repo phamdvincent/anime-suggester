c78d402a4933750cbbf7b4d6abb8617d1e19ddd5
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-01-26 11:10:13 +0000

    Added skeleton code

--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+
+def get_random_anime_list
+  api_url = "https://api.jikan.moe/v4/random/anime"
+  anime_list = []
+
+  3.times do
+    data = HTTP.get(api_url)
+    parsed_data = JSON.parse(data)
+    anime_data = parsed_data["data"]
+    anime_list.push(anime_data)
+  end
+  return anime_list
+
+end
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @anime_list = get_random_anime_list
+  erb(:random)
 end
